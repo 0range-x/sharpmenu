788652deb087f41c1e9de6eb31d2b73c321cb5f3
0range-x <1449760673@qq.com>
2023-03-06 22:49:55 +0800

    first commit

---
+++ startmenu/startMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace startmenu
+{
+    internal class startMenu
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+ 
+            foreach (var item in dirInfo.GetFileSystemInfos())
+            {
+                if (item.Attributes.HasFlag(FileAttributes.Directory) || item.Extension.Equals(".lnk"))
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+        }
+    }
+}
